ce0f96cc1e1fbf88e6e88f6721e3ff6bd421fd5b
GabrielOmogbolahan <53007773+omogbolahan94@users.noreply.github.com>
2023-09-05 02:56:11 +0000

    Hashed user password for security

--- db_py.py
+++ db_py.py
@@ -21,7 +21,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # database configuration
 db = SQLAlchemy(app)
-# db.init_app(app)
 
 
 with app.app_context():
@@ -35,47 +34,59 @@ with app.app_context():
         password = db.Column(db.String(100))
 
         def to_dict(self):
+            """
+            representing the user record in dictionary format
+            :return:
+            """
             dictionary = dict()
             for column in self.__table__.columns:
                 dictionary[column.name] = getattr(self, column.name)
             return dictionary
 
-
-        # # CONFIGURE TABLES
-        # class Event(db.Model):
-        #     __tablename__ = "events"
-        #     id = db.Column(db.Integer, primary_key=True)
-        #     # Create Foreign Key, "users.id". To reference user table with the event id.
-        #     event_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-        #     title = db.Column(db.String(250), unique=True, nullable=False)
-        #     date = db.Column(db.String(250), nullable=False)
-        #     description = db.Column(db.Text, nullable=False)
-        #
-        #
-        # # CONFIGURE TABLES
-        # class Booking(db.Model):
-        #     __tablename__ = "bookings"
-        #     id = db.Column(db.Integer, primary_key=True)
-        #     # Create Foreign Key, "users.id". To reference user table with the booking id.
-        #     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-        #     # Create Foreign Key, "users.id". To reference user table with the event id.
-        #     event_id = db.Column(db.Integer, db.ForeignKey("events.id"))
-        #     date_time = db.Column(db.DateTime, default=datetime.utcnow)
-
-
     # HTTP POST - Create Record
     @app.route("/register", methods=["POST"])
     def register():
-        new_user = User(
-            name=request.form.get("name"),
-            email=request.form.get("email"),
-            password=request.form.get("password")
+        # get user information from postman post
+        name = request.form.get("name"),
+        email = request.form.get("email"),
+        password = request.form.get("password")
+
+        # Check if the email already exists in the database
+        existing_user = User.query.filter_by(email=request.form.get("email")).first()
+
+        if existing_user:
+            return jsonify(response={"error": "Email already exists."}), 400
+
+        # # Check if user email is already present in the database.
+        # result = db.session.execute(db.select(User).where(User.email == request.form.get('email')))
+        # user = result.scalar()
+        #
+        # # User already exists?
+        # if user:
+        #     return "User already exists!"
+
+        # hash password for security purpose
+        hash_and_salted_password = generate_password_hash(
+            password,
+            method='pbkdf2:sha256',
+            salt_length=8
         )
+
+        # create user record using the post information with the User class model schema
+        new_user = User(
+            name=name[0],
+            email=email[0],
+            password=hash_and_salted_password
+        )
+
+        # add the user record to the user database
         db.session.add(new_user)
+
+        # commit the changes
         db.session.commit()
         return jsonify(response={"success": "Successfully added the new cafe."})
 
-
+    # GET: all users in json format
     @app.route('/all')
     def all_users():
         users = db.session.query(User).all()
