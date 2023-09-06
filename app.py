8722f654b21589b003487acf8d40ae29e4f45242
Rene <sambourene9@gmail.com>
2023-09-06 10:34:59 +0000

    added routes

--- app.py
+++ app.py
@@ -1,37 +1,69 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-import database
+from flask import Flask, jsonify,flash, request, render_template, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+import secrets
+import db_py
+
+from db_py import User
+
 
 app = Flask(__name__)
-app.secret_key = '333abc'
+
+app.config['SECRET_KEY'] = '29e91ce35b1f971ad0565d93a2a3777c'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///events_management.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User.query.filter_by(email=email).first()
+
+    if user and check_password_hash(user.password, password):
+        login_user(user)
+        flash('Login successful!')
+        return redirect(url_for('index'))
+    else:
+        flash('Login failed. Please check your email and password.')
+        return redirect(url_for('signin'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('Logout successful!')
+    return redirect(url_for('index'))
+
+
+@app.route('/signup')
+def signup():
     return render_template('signup.html')
 
-@app.route('/submit', methods = ['POST'])
-def dashboard():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    if username in database.users_database and password == database.users_database[username]:
-        return render_template('dashboard.html', username=username)
-    else:
-        flash('Login failed. Please check your username and password.')
-        return redirect('/')
-    
-@app.route('/logout', methods = ['POST'])
-def logout():
-    return redirect('/')
-        
-    
-def bolden(func):
-    def wrap():
-        bold = func()
-        return f'<b>{bold}<b>'
-    return wrap
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = request.form.get("password")
 
-@app.route('/greeting/<name>/<int:age>')
-def greeting(name, age):
-    return f'<p>Hello, {age} year old {name} </p>'
+    existing_user = User.query.filter_by(email=email).first()
+
+    if existing_user:
+        return jsonify(response={"error": "Email already exists."}), 400
+
+    hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+    new_user = User(name=name, email=email, password=hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(response={"success": "Registration successful."})
 
 
 if __name__ == "__main__":
