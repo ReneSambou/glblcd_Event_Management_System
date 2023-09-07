15ceff917b19e51e515ae9de5d3f3a5dc6e6fcc4
coderman2 <okquaye@st.ug.edu.gh>
2023-09-07 10:20:40 +0000

    Fixed database issue

--- models.py
+++ models.py
@@ -23,7 +23,7 @@ class User(db.Model):
             return dictionary
         
 class Event(db.Model):
-        __tablename__ = "events"
+        __tablename__ = "Event"
 
         id = db.Column(db.Integer, primary_key=True)
         user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
